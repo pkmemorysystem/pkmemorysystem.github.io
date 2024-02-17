20bbf159e60b62e33c0c8a712ea11ad3ec16db2c
Absideon <absideoncollective@email.com>
2024-02-17 15:39:56 +0100

    Better Buttons

--- Scripts/members.js
+++ Scripts/members.js
@@ -160,12 +160,20 @@ function displayMembers(members) {
     memberImg.alt = member.name + "'s avatar";
     memberImg.draggable = false;
     
-    const x = document.createElement("span");
-    x.textContent = "x";
-    x.style.color = "red";
-    x.style.opacity = "0.25";
-    x.style.cursor = "pointer";
-    x.style.userSelect = "none";
+    const x = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    x.classList.add("btn");
+    x.style.height = "2em";
+    x.style.width = "2em";
+    x.setAttribute("clip-rule", "evenodd");
+    x.setAttribute("fill-rule", "evenodd");
+    x.setAttribute("stroke-linejoin", "round");
+    x.setAttribute("stroke-miterlimit", "2");
+    x.setAttribute("viewBox", "0 0 24 24");
+
+    const xPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    xPath.style.opacity = "0.25";
+    xPath.setAttribute("fill", "red");
+    xPath.setAttribute("d", "m12 10.93 5.719-5.72c.146-.146.339-.219.531-.219.404 0 .75.324.75.749 0 .193-.073.385-.219.532l-5.72 5.719 5.719 5.719c.147.147.22.339.22.531 0 .427-.349.75-.75.75-.192 0-.385-.073-.531-.219l-5.719-5.719-5.719 5.719c-.146.146-.339.219-.531.219-.401 0-.75-.323-.75-.75 0-.192.073-.384.22-.531l5.719-5.719-5.72-5.719c-.146-.147-.219-.339-.219-.532 0-.425.346-.749.75-.749.192 0 .385.073.531.219z");    
 
     const memberName = document.createElement("h3");
     memberName.textContent = member.name;
@@ -177,6 +185,7 @@ function displayMembers(members) {
     memberColor.classList.add("member-color");
     memberColor.style.backgroundColor = "#" + (member.color || "00000030"); // Default color black
     
+    x.appendChild(xPath);
     memberDiv.appendChild(x);
     memberDiv.appendChild(memberSelf);
     memberDiv.appendChild(memberImg);
@@ -234,11 +243,11 @@ function displayMembers(members) {
     frontMemberList.appendChild(memberTab);
 
     x.addEventListener("mouseover", () => {
-      x.style.opacity = "1";
+      xPath.style.opacity = "1";
     });
     
     x.addEventListener("mouseout", () => {
-      x.style.opacity = "0.25";
+      xPath.style.opacity = "0.25";
     });
 
     const memberId = member.id;
